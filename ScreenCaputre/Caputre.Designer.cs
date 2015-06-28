c15583c94e1ee373885a88e55a9c079701f94f4f
gungume <gungume@gmail.com>
2015-06-28 16:03:28 +0900

    전체화면 캡쳐 및 저장
    http://gungume.com/119

---
+++ ScreenCaputre/Caputre.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace ScreenCaputre
+{
+    partial class Caputre
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.mainMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.captureMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.menu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mainMenu});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(284, 24);
+            this.menu.TabIndex = 3;
+            this.menu.Text = "menuStrip1";
+            // 
+            // mainMenu
+            // 
+            this.mainMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.captureMenu,
+            this.saveMenu,
+            this.toolStripSeparator1,
+            this.exitMenu});
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.Size = new System.Drawing.Size(50, 20);
+            this.mainMenu.Text = "Menu";
+            // 
+            // captureMenu
+            // 
+            this.captureMenu.Name = "captureMenu";
+            this.captureMenu.Size = new System.Drawing.Size(116, 22);
+            this.captureMenu.Text = "Capture";
+            this.captureMenu.Click += new System.EventHandler(this.captureMenu_Click);
+            // 
+            // saveMenu
+            // 
+            this.saveMenu.Name = "saveMenu";
+            this.saveMenu.Size = new System.Drawing.Size(116, 22);
+            this.saveMenu.Text = "Save";
+            this.saveMenu.Click += new System.EventHandler(this.saveMenu_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(113, 6);
+            // 
+            // exitMenu
+            // 
+            this.exitMenu.Name = "exitMenu";
+            this.exitMenu.Size = new System.Drawing.Size(116, 22);
+            this.exitMenu.Text = "Exit";
+            this.exitMenu.Click += new System.EventHandler(this.exitMenu_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(284, 240);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox.TabIndex = 4;
+            this.pictureBox.TabStop = false;
+            // 
+            // Caputre
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 264);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.menu);
+            this.Name = "Caputre";
+            this.Text = "Windows Screen Capture";
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem mainMenu;
+        private System.Windows.Forms.ToolStripMenuItem captureMenu;
+        private System.Windows.Forms.ToolStripMenuItem saveMenu;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitMenu;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ ScreenCaputre/Caputre.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace ScreenCaputre
+{
+    public partial class Caputre : Form
+    {
+        Bitmap bitmap = null;
+        public Caputre()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 윈도우 전체화면 캡쳐.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void captureMenu_Click(object sender, EventArgs e)
+        {
+            int width = Screen.PrimaryScreen.Bounds.Width;
+            int height = Screen.PrimaryScreen.Bounds.Height;
+
+            Size size = new Size(width, height);
+            bitmap = new Bitmap(width, height);
+            Graphics g = Graphics.FromImage(bitmap);
+            g.CopyFromScreen(0, 0, 0, 0, size);
+
+            pictureBox.Image = bitmap;
+        }
+
+        /// <summary>
+        /// 캡쳐된 화면 파일로 저장.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void saveMenu_Click(object sender, EventArgs e)
+        {
+            if (bitmap != null)
+            {
+                saveFileDialog.Filter = "JPEG|*.jpg|BMP|*.bmp|PNG|*.png";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    switch (saveFileDialog.FilterIndex)
+                    {
+                        case 1:
+                            bitmap.Save(saveFileDialog.FileName, ImageFormat.Jpeg);
+                            break;
+                        case 2:
+                            bitmap.Save(saveFileDialog.FileName, ImageFormat.Bmp);
+                            break;
+                        case 3:
+                            bitmap.Save(saveFileDialog.FileName, ImageFormat.Png);
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Caputre first.", "Error");
+            }
+        }
+
+        private void exitMenu_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
